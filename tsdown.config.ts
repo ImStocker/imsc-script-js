4989616b014d0f1eeb25c2e9fdc963b4de5ef8f3
Nordth <emo-name@hotmail.com>
2026-04-02 18:21:21 +0400

    Add build for browser

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-  entry: [
-    'src/index.ts',
-    'src/Props.ts',
-    'src/Graph.ts'
-  ],
-  dts: {
-    tsgo: true,
+export default defineConfig([
+  {
+    entry: [
+      'src/index.ts',
+      'src/Props.ts',
+      'src/Graph.ts'
+    ],
+    outDir: 'dist',
+    format: ['esm', 'cjs'],
+    splitting: true,
+    dts: {
+      tsgo: true,
+    },
+    exports: true,
+    clean: true,
   },
-  exports: true,
-  // ...config options
-})
+  {
+    entry: ['src/index.ts'],
+    outDir: 'dist-browser',
+    format: ['iife'],
+    globalName: 'ImscScript',
+    splitting: false,
+    minify: true,
+  }
+])
